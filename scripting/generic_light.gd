73be6b837a3e672d245bb7d31ade33b421dc0b2b
Richard Snider <lighthousemaniac@gmail.com>
2026-04-18 20:16:29 -0600

    add a packed light scene based on the components in TestScene

---
+++ scripting/generic_light.gd
@@ -0,0 +1,14 @@
+class_name GenericLight extends Node
+
+
+@onready var light_node: SpotLight3D = get_node("light")
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
